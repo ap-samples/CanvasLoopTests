5b39b99169132e58e859ccd75c37a39237e36a11
Arthur <artur.peka@gmail.com>
2014-06-24 17:46:23 +0300

    fixing the image/text being struck when it reached the end of canvas + general cleanup

--- js/main_img_old.js
+++ js/main_img_old.js
@@ -3,6 +3,7 @@ const FRAME_REQ_INTERVAL_MS = 1000/FPS;
 
 var canv = document.getElementById("main-canv");
 var ctx = canv.getContext("2d");
+var moveForward = true;
 
 var imgX = 10;
 var imgY = 20;
@@ -11,7 +12,12 @@ var bugImage = loadImage('img/bug.png');
 
 function doFrame(){
 	ctx.clearRect(0,0,canv.width, canv.height);
-	((imgX+30) < imgXMax) ? imgX += 2 : imgX -= 2;
+	if((imgX+30) >= imgXMax)
+		moveForward = false;
+	else if((imgX + 30) <= 0)
+		moveForward = true;
+	
+	(moveForward) ? imgX += 1 : imgX -= 1;
 	ctx.drawImage(bugImage, imgX, imgY);
 }
 
--- js/main_text_old.js
+++ js/main_text_old.js
@@ -3,6 +3,7 @@ const FRAME_REQ_INTERVAL_MS = 1000/FPS;
 
 var canv = document.getElementById("main-canv");
 var ctx = canv.getContext("2d");
+var moveForward = true;
 
 var textX = 10;
 var textY = 20;
@@ -10,7 +11,11 @@ const textXMax = canv.width;
 
 function doFrame(){
 	ctx.clearRect(0,0,canv.width, canv.height);
-	((textX+30) < textXMax) ? textX += 2 : textX -= 2;
+	if ((textX+30) >= textXMax)
+		moveForward = false;
+	else if((textX+30) <= 0)
+		moveForward = true;
+	(moveForward) ? textX += 2 : textX -= 2;
 	ctx.fillText("This is test",textX,textY);
 }
 
